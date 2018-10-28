392e1a86e4e4b491d5557a881b2a96929681ed30
mapuaptu <mapuaptu@gmail.com>
2018-10-28 14:31:09 +0400

    Пересобрал user_app

---
+++ user_app/build/precache-manifest.c49f5f161b18401801bd4b7b1c7827f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80069b4727dda66546dc1aebf11dae6d",
+    "url": "/static/media/user.80069b47.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2eca1240d5507cb1f46b",
+    "url": "/static/js/main.2eca1240.chunk.js"
+  },
+  {
+    "revision": "889dce23f9ebbdac6e59",
+    "url": "/static/js/1.889dce23.chunk.js"
+  },
+  {
+    "revision": "c2e72ea5a86d8c671197b913d83150da",
+    "url": "/index.html"
+  }
+];
